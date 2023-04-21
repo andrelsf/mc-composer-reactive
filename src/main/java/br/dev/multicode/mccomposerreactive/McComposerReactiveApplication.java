1c145652c0183ce2dd79ecc933b28983ebe9c0f9
Andre Ferreira <andre.ferreira@multicode.dev.br>
2023-04-21 13:59:08 -0300

    Initial commit

---
+++ src/main/java/br/dev/multicode/mccomposerreactive/McComposerReactiveApplication.java
@@ -0,0 +1,13 @@
+package br.dev.multicode.mccomposerreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McComposerReactiveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(McComposerReactiveApplication.class, args);
+  }
+
+}
